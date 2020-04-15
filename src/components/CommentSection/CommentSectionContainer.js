9b10642ae958d0bb845848d85fb5a893554c055b
Sean Kramer <seandominickramer@gmail.com>
2020-04-14 20:08:22 -0400

    Correct variables in comment section

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,47 +1,46 @@
-// // You will add code to this file
-// import React, { useState } from "react";
-// import CommentInput from "./CommentInput";
-// import Comment from "./Comment";
-// import "./Comment.css";
+// You will add code to this file
+/*import React, { useState } from "react";
+import CommentInput from "./CommentInput";
+import Comment from "./Comment";
+import "./Comment.css";
 
-// const CommentSection = props => {
-//   // Add state for the comments
+const CommentSection = props => {
+  // Add state for the comments
 // const [comments, setComments] = useState(props.comments)
-//   return (
-//     <div>
-//       <Comment />
-//       {/* map through the comments data and return the Comment component */}
-//       {comments.map((comment, index) =>
-//       {
-//         return <Comment key={index} comment={comment}/>
-//       }
-
-//       )
-//       }
+  return (
+    <div>
+      <Comment />
+      {/* map through the comments data and return the Comment component */
+     /* {props.map(comment => <Comment key={props.username} comment={}
+      
+      }
 
 
-//       <CommentInput />
-//     </div>
-//   );
-// };
+      <CommentInput />
+    </div>
+  );
+};
 
-// export default CommentSection;
+export default CommentSection;*/
+
+
 // You will add code to this file
 import React from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
-const CommentSection = (props) => {
-  const {comments} = props
-  return (
+ const CommentSection = (props) =>
+{
+   const {comments} = props
+   return (
     <div>
       {/* map through the comments data and return the Comment component */}
       {
-        // information.forEach(item => <Comment comment={item}/>)
+         // information.forEach(item => <Comment comment={item}/>)
         comments.map(comment => <Comment key={comment.username} comment={comment}/>)
-      }
-      <CommentInput />
-    </div>
-  );
+       }
+       <CommentInput />
+   </div>
+ );
 };
-export default CommentSection;
+ export default CommentSection;
